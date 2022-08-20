6a5c61898649aed13c6d28467fb6407d92d848a0
adrianfante <adrianfante@gmail.com>
2022-08-20 12:08:42 -0300

    clase 3

--- manipulando-dom-con-js-proyecto_inicial/script.js
+++ manipulando-dom-con-js-proyecto_inicial/script.js
@@ -3,9 +3,21 @@ const createTask =(evento) => {
 	//console.log(evento);
 	evento.preventDefault();
 	const input = document.querySelector('[data-form-input]');
-	console.log(input.value);
-	//console.log("crear tarea");
-
+	const value = input.value;
+	const list =document.querySelector('[data-list]');
+	input.value = "";
+	const task = document.createElement('li');
+	task.classList.add('card');
+	const content = `
+            <div>
+              <i class="far fa-check-square icon"></i>
+              <span class="task">${value}</span>
+            </div>
+            <i class="fas fa-trash-alt trashIcon icon"></i>
+`;
+	task.innerHTML = content;
+	list.appendChild(task)
+	console.log(content);
 };
 
 btn.addEventListener("click", createTask);
